d070bbecd824be067a89f91ffc550f4c7e2988ce
open-the-light <a.j.eckersall@gmail.com>
2025-08-31 15:19:30 +0900

    updated SLR model file

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -1,34 +1,48 @@
 import pandas as pd
 
 class SimpleLinearRegression:
-    def __init__(self) -> None:
-        self.w_estimate = 0
-        self.b_estimate = 0
+    def __init__(self, data: pd.DataFrame, x_name: str, y_name: str) -> None:
+        self.w = 0
+        self.b = 0
+        self.alpha = 0.01
+
+        self.x = data[x_name]
+        self.y = data[y_name]
+
+        self.m = len(self.x)
 
     def get_parameter_estimates(self) -> tuple[float, float]:
-        return self.w_estimate, self.b_estimate
+        return self.w, self.b
 
-    def calculate_cost(self, x: pd.Series, y: pd.Series, w: float, b: float) -> float:
-        m = len(x)
+    def calculate_cost(self) -> float:
+        cost = sum(((self.w * self.x + self.b) - self.y)**2)
+        return cost / (2*self.m)
 
-        cost = 0
+    def calculate_gradients(self) -> tuple[float, float]:
+        
+        dj_dw = sum(((self.w * self.x + self.b) - self.y) * self.x)
+        dj_db = sum((self.w * self.x + self.b) - self.y)
+        
+        return (dj_dw / self.m, dj_db / self.m)
 
-        for i in range(m):
-            cost += ((w * x[i] + b) - y[i])**2
-        return cost / 2*m
+    def train_model(self, w: float = 0, b: float = 0, alpha: float = 0.01, n_iters: int = 10000) -> None:
 
-    def calculate_gradients(self, x: pd.Series, y: pd.Series, w: float, b: float) -> tuple[float, float]:
-        return 0, 0
+        self.w = w
+        self.b = b
+        self.alpha = alpha
 
-    def gradient_descent(self):
-        pass
+        iter = 1
+        while iter <= n_iters:
+            dj_dw, dj_db = self.calculate_gradients()
 
-    def train_model(self, data: pd.DataFrame, x_name: str, y_name: str, n_iters: int) -> None:
-        x = data[x_name]
-        y = data[y_name]
+            self.w = self.w - self.alpha * dj_dw
+            self.b = self.b - self.alpha * dj_db
+            
+            if iter % 100 == 0:
+                print(f"Iter {iter}: w = {self.w}, b = {self.b}, cost = {self.calculate_cost()}")
+            iter += 1
+                
 
-        print(x)
-        print(y)
 
     def predict(self, x: pd.DataFrame):
         pass
--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from SimpleLinearRegression import SimpleLinearRegression
 import sklearn.datasets
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     boston = pd.read_csv("datasets/Boston.csv", index_col=0)
-    print(boston.head())
-    print(boston.describe())
 
-    lr_model = SimpleLinearRegression()
-    lr_model.train_model(boston, "age", "medv", 10000)
+    var = "dis"
+    plt.scatter(boston[var], boston["medv"])
+    plt.show()
+
+    test_data = pd.DataFrame(
+        {
+            'x': [1.0, 2.0],
+            'y': [300.0, 500.0]
+        }
+    )
+
+    lr_model = SimpleLinearRegression(boston, var, "medv")
+    lr_model.train_model(n_iters=1000)
+
+    w, b, = lr_model.get_parameter_estimates()
+
+    x = np.linspace(0, 15, 100)
+    plt.scatter(boston[var], boston["medv"])
+    plt.plot(x, w * x + b, "-r")
+    plt.show()
 
 
 if __name__ == "__main__":
